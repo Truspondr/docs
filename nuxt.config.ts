429c07c4bba56f2a8166898ee63a5ee86640df4a
whxpop <abi@whxpop.net>
2024-08-23 11:11:10 +0000

    removed metrics package

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,9 +5,6 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
 
   modules: [
-    // Remove it if you don't use Plausible analytics
-    // https://github.com/nuxt-modules/plausible
-    '@nuxtjs/plausible'
   ],
 
   compatibilityDate: '2024-08-23'
